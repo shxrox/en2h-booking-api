1016573b0713af29a2ca207121eb902b630685c9
shxrox <sharondeva8@gmail.com>
2026-07-11 09:37:15 +0530

    feat: configure swagger api documentation and add decorators to auth controller

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
@@ -17,6 +18,17 @@ async function bootstrap() {
 
   // Enable global error formatting
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  // Swagger API Documentation Setup
+  const config = new DocumentBuilder()
+    .setTitle('EN2H Booking API')
+    .setDescription('The REST API documentation for the EN2H Booking Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
